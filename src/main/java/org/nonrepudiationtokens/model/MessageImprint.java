3178ae6ea4135db7a183573a871530bd68feb479
Brian Russell <brussell2@gmail.com>
2026-03-11 10:32:57 -0700

    Update MessageImprint.java

--- src/main/java/org/nonrepudiationtokens/model/MessageImprint.java
+++ src/main/java/org/nonrepudiationtokens/model/MessageImprint.java
@@ -1,1 +1,26 @@
+package org.nonrepudiationtokens.model;
 
+import java.util.Arrays;
+import java.util.Objects;
+
+public final class MessageImprint {
+
+    private final HashAlgorithm hashAlgorithm;
+    private final byte[] hashValue;
+
+    public MessageImprint(HashAlgorithm hashAlgorithm, byte[] hashValue) {
+        this.hashAlgorithm = Objects.requireNonNull(hashAlgorithm, "hashAlgorithm must not be null");
+        this.hashValue = Arrays.copyOf(
+                Objects.requireNonNull(hashValue, "hashValue must not be null"),
+                hashValue.length
+        );
+    }
+
+    public HashAlgorithm getHashAlgorithm() {
+        return hashAlgorithm;
+    }
+
+    public byte[] getHashValue() {
+        return Arrays.copyOf(hashValue, hashValue.length);
+    }
+}
