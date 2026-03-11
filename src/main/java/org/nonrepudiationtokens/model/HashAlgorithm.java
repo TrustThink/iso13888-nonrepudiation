fbf4a9ea1c964f49b23c26193c9f3ff43999c4d1
Brian Russell <brussell2@gmail.com>
2026-03-11 10:32:36 -0700

    Update HashAlgorithm.java

--- src/main/java/org/nonrepudiationtokens/model/HashAlgorithm.java
+++ src/main/java/org/nonrepudiationtokens/model/HashAlgorithm.java
@@ -1,1 +1,33 @@
+package org.nonrepudiationtokens.model;
 
+public enum HashAlgorithm {
+
+    SHA256("SHA-256", 0),
+    SHA384("SHA-384", 1),
+    SHA512("SHA-512", 2);
+
+    private final String jcaName;
+    private final int asn1Value;
+
+    HashAlgorithm(String jcaName, int asn1Value) {
+        this.jcaName = jcaName;
+        this.asn1Value = asn1Value;
+    }
+
+    public String getJcaName() {
+        return jcaName;
+    }
+
+    public int getAsn1Value() {
+        return asn1Value;
+    }
+
+    public static HashAlgorithm fromAsn1Value(int value) {
+        for (HashAlgorithm algorithm : values()) {
+            if (algorithm.asn1Value == value) {
+                return algorithm;
+            }
+        }
+        throw new IllegalArgumentException("Unsupported ASN.1 HashAlgorithm value: " + value);
+    }
+}
