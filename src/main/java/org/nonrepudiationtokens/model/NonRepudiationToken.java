b098c24a01ddf7199872ae82056007c09d08e3ca
Brian Russell <brussell2@gmail.com>
2026-03-11 10:33:12 -0700

    Update NonRepudiationToken.java

--- src/main/java/org/nonrepudiationtokens/model/NonRepudiationToken.java
+++ src/main/java/org/nonrepudiationtokens/model/NonRepudiationToken.java
@@ -1,1 +1,18 @@
+package org.nonrepudiationtokens.model;
 
+import java.time.Instant;
+
+public interface NonRepudiationToken {
+
+    String getPolicyId();
+
+    String getOriginatorId();
+
+    String getRecipientId();
+
+    Instant getTokenGenerationTime();
+
+    MessageImprint getSubjectImprint();
+
+    byte[] getContextInfo();
+}
